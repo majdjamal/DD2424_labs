f7889044b2f01f2ebfcf3b5953553c120e909af7
majdjamal <majdj@kth.se>
2021-04-28 18:45:34 +0200

    Lab3

--- LAB3/src/model.py
+++ LAB3/src/model.py
@@ -22,48 +22,69 @@ class ConvNet:
         x = x.reshape((height, width), order='F')
         return x
 
-    def MakeMFMatrix(self, X, F, NLongest):
+    def MakeMFMatrix(self, X, F):
+        """ Generates the MF matrix for a data point
+        :param X: data point, shape = (Nunique, NLongest)
+        :param F: filter, shape = (Height, Width)
+        :return MF: The MF matrix
+        """
 
         height_x, width_x = X.shape
-        depth_f, height_f, width_f = F.shape
-
-        ind_F = np.arange(height_f*width_f)
+        df, height_f, width_f = F.shape
 
         FF = F.flatten().reshape(-1, 1)
+        F_entries = height_f* width_f
 
         MF = np.zeros((
         (width_x - width_f + 1)*(height_x - height_f + 1),
-         width_x*height_x))
 
-        vec = MF[0]
+        width_x*height_x))
 
-        jump = width_x - width_f    # >> 3
+        shift = 0
 
-        for i in range((width_x - width_f + 1)*(height_x - height_f + 1)):
+        for k in range(height_x - height_f + 1):
 
-            curr_F = np.zeros((width_x*height_x))
-            curr_jump = 0
 
-            for j in range(ind_F.size):
 
-                if j % width_f == 0 and j != 0:
-                    curr_jump += jump
+            for i in range(width_x - width_f + 1):
 
-                index = j + curr_jump + i
+                jump = 0
+                MF_row = np.zeros(MF.shape[1])
 
-                curr_F[index] = FF[j]
+                for j in range(F_entries):
 
-            MF[i] = curr_F
+                    if j % width_f == 0 and j != 0:
+
+                        jump += width_x - width_f
+
+                    ind = j + jump + i + shift
+                    #print(ind)
+
+                    MF_row[ind] = FF[j]
+
+                #print('\n')
+                row = k * (width_x - width_f + 1) + i
+                MF[row] = MF_row
+
+            shift += width_x
+
+                    #MF_row[ind] = FF[j]
 
         return MF
 
-    def MakeMXMatrix(self, X, F):
 
+    def MakeMXMatrix(self, X, F):
+        """ Generates the MX-matrix
+        :param X: data point, shape = (Nunique, NLongest)
+        :param F: filter, shape = (Height, Width)
+        :return MX: The MX matrix
+        """
         hx, wx = X.shape
         df, hf, wf  = F.shape
 
         ind_rows = np.arange(hf + 1)
         ind_cols = np.arange(wf + 1)
+
 
         def MX(x):
 
@@ -76,8 +97,8 @@ class ConvNet:
                     (ind_rows[0] + i):(ind_rows[-1] + i), #rows
                     (ind_cols[0] + j):(ind_cols[-1] + j)] #columns
 
-                    row = j + i * (wx - wf + 1)
-                    MX[i] = vals.flatten()
+                    row = j + (i)*(wx - wf + 1)
+                    MX[row] = vals.flatten()
 
             return MX
 
@@ -91,7 +112,6 @@ class ConvNet:
 
         return MX_tot
 
-
     def fit(self, data, params):
 
         X = data.X
@@ -100,33 +120,26 @@ class ConvNet:
 
         Ndim, Npts = X.shape
         Nout = Y.shape
-        x_input = self.convert2matrix(X[:, 1], data.NUnique, data.NLongest)
 
-        F1_TEST = np.random.random((1, 3, 3))
 
         F1 = np.random.random((params.NF1, data.NUnique, params.widthF1))
         F2 = np.random.random((params.NF2, params.NF1, params.widthF2))
-
         W = np.random.random((data.NClasses, Npts))
 
-        MX1 = self.MakeMXMatrix(x_input, F1_TEST)
-        MF1 = self.MakeMFMatrix(x_input, F1_TEST, 0)
+        F = np.random.random((1, 28, 6))
+        x_input = self.convert2matrix(X[:, 0], data.NUnique, data.NLongest)
 
-        #np.savetxt('MX', MX1)
-        #np.savetxt('X', x_input)
+        MX = self.MakeMXMatrix(x_input, F)
+        MF = self.MakeMFMatrix(x_input, F)
 
+        print(MF)
 
-
-        print(MX1)
-        print(1 in MX1)
-        """
-        s1 = MX1 @ F1_TEST.flatten().reshape(-1, 1)
-
-        s2 = MF1 @ X[:, 1]
-
+        s1 = MX @ F.flatten()
+        s2 = MF @ x_input.flatten()
         print(s1, s2)
+        print(np.all(s1 == s2))
 
-        """
+
         #toVec = self.convert2vec(X[:, 1000], data.NUnique, data.NLongest)
         #print(data.names[1000])
 
